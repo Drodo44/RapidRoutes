7974a5dc7bf4457e464a9fd36f95f194852707f0
Drodo44 <AJC4492@gmail.com>
2025-07-27 00:17:52 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css}",    // <-- add this so globals.css is scanned
   ],
   theme: {
     extend: {
