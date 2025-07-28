177a782bbf74507ba42d3c9736e7c30ac0f53e8d
Drodo44 <AJC4492@gmail.com>
2025-07-27 20:27:46 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,6 @@
-import '../styles/globals.css';
-import { useEffect, useState } from 'react';
-import supabase from '../utils/supabaseClient';
-import { useRouter } from 'next/router';
+// pages/_app.js
+import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    supabase.auth.getUser().then(({ data: { user } }) => {
-      setUser(user);
-    });
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user || null);
-    });
-    return () => {
-      listener.subscription.unsubscribe();
-    };
-  }, []);
-
-  useEffect(() => {
-    const publicRoutes = ['/login', '/signup'];
-    if (!user && !publicRoutes.includes(router.pathname)) {
-      router.push('/login');
-    }
-  }, [user, router.pathname]);
-
-  return <Component {...pageProps} user={user} />;
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
