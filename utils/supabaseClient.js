fa5a8bb87fb63bf72568a58eced3016f4969ad0b
Drodo44 <AJC4492@gmail.com>
2025-07-28 11:36:11 -0400

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,10 +1,7 @@
-// utils/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase }; // named export
-export default supabase; // default export
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+export { supabase };
+export default supabase;
