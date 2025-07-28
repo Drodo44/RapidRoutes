e278c69f4ad6f8a1df9f922498aac57494f78103
Drodo44 <AJC4492@gmail.com>
2025-07-28 11:01:39 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,10 @@
-// pages/_document.js
+// /pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Tailwind via CDN */}
         <link
           href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css"
           rel="stylesheet"
