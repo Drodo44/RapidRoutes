a2bf04727ed6755680dae8f2f9c360e91b7b1577
Drodo44 <AJC4492@gmail.com>
2025-07-31 15:08:41 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,11 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark">
+    <Html lang="en">
       <Head>
-        <meta name="description" content="RapidRoutes – Redefine the Game. Outsmart the Lane." />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#0f172a" />
       </Head>
       <body className="bg-gray-950 text-white">
         <Main />
