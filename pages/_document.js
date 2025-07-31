e1e236b3398e5edae577affbf262678ccdb84f99
Drodo44 <AJC4492@gmail.com>
2025-07-31 11:51:59 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,12 @@
-// /pages/_document.js
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="dark">
       <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css"
-          rel="stylesheet"
-        />
+        <meta name="description" content="RapidRoutes – Redefine the Game. Outsmart the Lane." />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="bg-gray-950 text-white">
         <Main />
