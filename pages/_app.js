4630297b5ab8318f6c65fd796a5a0a6f38140ad3
Drodo44 <AJC4492@gmail.com>
2025-07-31 11:51:46 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
-// /pages/_app.js
+// pages/_app.js
+import { useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
 import "../styles/globals.css";
-import Layout from "../components/Layout";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const {
+      data: { subscription }
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_IN") {
+        console.log("User signed in", session);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
-    <Layout>
+    <main className="min-h-screen bg-gray-950 text-white">
       <Component {...pageProps} />
-    </Layout>
+    </main>
   );
 }
+
+export default MyApp;
