19b3ac7dc2962b0fc93f484d7b1db80fe1eff5c6
Drodo44 <AJC4492@gmail.com>
2025-08-01 16:25:43 -0400

    Create fetchCities.js

---
+++ utils/fetchCities.js
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://gwhjxomavulwduhvgvi.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3dWhqeG9tYXZ1bHdkdWh2Z3ZpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTkzOTYyOSwiZXhwIjoyMDY3NTE1NjI5fQ.vYLGwjNHHPhJZPnemRAhlWXAYKrKH--9BOzfe6TWQVQ"
+);
+
+export default async function fetchCitiesFromSupabase() {
+  const { data, error } = await supabase
+    .from("cities")
+    .select("city, state_or_province, postal_code, latitude, longitude, kma, kma_name, region, freight_score");
+
+  if (error) throw new Error(error.message);
+  return data;
+}
