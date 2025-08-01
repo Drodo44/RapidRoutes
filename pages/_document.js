d85272e9cce58c184fdfd9831fc18e189994f1b1
Drodo44 <AJC4492@gmail.com>
2025-08-01 13:21:25 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#0f172a" />
-      </Head>
+      <Head />
       <body className="bg-gray-950 text-white">
         <Main />
         <NextScript />
