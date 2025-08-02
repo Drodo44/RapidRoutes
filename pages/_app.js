3842690e9f04cc10f64751343a397b38512ff3dd
Drodo44 <AJC4492@gmail.com>
2025-08-02 13:13:21 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,19 @@
 // pages/_app.js
 import "../styles/globals.css";
+import TopNav from "../components/TopNav";
 import { useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
-import TopNav from "../components/TopNav";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    const { data: authListener } = supabase.auth.onAuthStateChange(() => {});
-    return () => authListener?.subscription.unsubscribe();
+    const { data: listener } = supabase.auth.onAuthStateChange(() => {});
+    return () => listener?.subscription.unsubscribe();
   }, []);
 
   return (
     <main className="min-h-screen bg-gray-950 text-white">
       <TopNav />
-      <div className="px-6 py-4">
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </main>
   );
 }
