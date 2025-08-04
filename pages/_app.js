711929cec5eb2b7e472db778c10772bdf6a90389
Drodo44 <AJC4492@gmail.com>
2025-08-04 11:54:26 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 // pages/_app.js
 import "../styles/globals.css";
-import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <style>{`
-          /* force navbar links to off-white, indigo on hover */
-          .navbar a       { color:#E2E8F0 !important; }
-          .navbar a:hover { color:#4361EE !important; }
-        `}</style>
-      </Head>
+    <main className="min-h-screen bg-gray-950 text-white font-sans">
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
