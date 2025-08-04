fe258a0a5fee605a680c0feb6df8652ae5fe4155
Drodo44 <AJC4492@gmail.com>
2025-08-03 21:18:13 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "../styles/globals.css";   // ← relative path; works out-of-the-box
+import "../styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
@@ -7,9 +7,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <style>{`
-          /* Navbar link color override */
-          .navbar a { color: #E2E8F0 !important; }
-          .navbar a:hover { color: #4361EE !important; }
+          /* force navbar links to off-white, indigo on hover */
+          .navbar a       { color:#E2E8F0 !important; }
+          .navbar a:hover { color:#4361EE !important; }
         `}</style>
       </Head>
       <Component {...pageProps} />
