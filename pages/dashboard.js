419d3b8bde4a764cffecda57cb9edfce3323796e
Drodo44 <AJC4492@gmail.com>
2025-08-03 23:04:13 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar.js';
 import FloorSpaceChecker from '../components/FloorSpaceChecker';
 
 export default function Dashboard() {
