0d433089c23130105193816dfac1c63ab93887cb
Drodo44 <AJC4492@gmail.com>
2025-08-06 22:17:57 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 // pages/_app.js
-import "../styles/globals.css";
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main className="min-h-screen bg-gray-950 text-white font-sans">
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <style>{`
+          body {
+            background-color: #0f172a;
+            color: #ffffff;
+          }
+        `}</style>
+      </Head>
+      <main className="min-h-screen bg-gray-950 text-white">
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
