6d9460d95d0bd879fa9726d547ba9f77e8536e57
Drodo44 <AJC4492@gmail.com>
2025-08-13 09:08:24 -0400

    Create datHeaders.test.js

---
+++ tests/datHeaders.test.js
@@ -0,0 +1,35 @@
+// tests/datHeaders.test.js
+import { DAT_HEADERS } from "../lib/datCsvBuilder.js";
+
+const EXPECT = [
+  "Pickup Earliest*",
+  "Pickup Latest*",
+  "Length (ft)*",
+  "Weight (lbs)*",
+  "Full/Partial*",
+  "Equipment*",
+  "Use Private Network*",
+  "Private Network Rate",
+  "Allow Private Network Booking",
+  "Allow Private Network Bidding",
+  "Use DAT Load Board*",
+  "DAT Load Board Rate",
+  "Allow DAT Load Board Booking",
+  "Use Extended Network",
+  "Contact Method*",
+  "Origin City*",
+  "Origin State*",
+  "Origin Postal Code",
+  "Destination City*",
+  "Destination State*",
+  "Destination Postal Code",
+  "Comment",
+  "Commodity",
+  "Reference ID (unique per organization; max 8 chars)",
+];
+
+describe("DAT header order", () => {
+  it("matches spec exactly", () => {
+    expect(DAT_HEADERS).toEqual(EXPECT);
+  });
+});
