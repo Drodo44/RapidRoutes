6a3af6332e22432027c49d0a19801c18d944fda9
Drodo44 <AJC4492@gmail.com>
2025-08-13 13:03:20 -0400

    Update and rename next.config.mjs to tailwind.config.cjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // If you later switch to next/image, add allowed domains here via `images.domains`.
-};
-
-export default nextConfig;
