6484cea3572a1a426d8666315871f214d2049ea5
Drodo44 <AJC4492@gmail.com>
2025-08-13 13:02:22 -0400

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // If you later switch to next/image, add allowed domains here via `images.domains`.
+};
+
+export default nextConfig;
