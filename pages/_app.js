e08aad99cf59c827795c9a9df97f16a7265224cf
Drodo44 <AJC4492@gmail.com>
2025-08-18 09:57:48 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 // pages/_app.js
 import "../styles/globals.css";
+import Nav from "../components/Nav";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const AUTH_PAGES = ["/login", "/signup", "/register", "/reset-password", "/auth/callback"];
+
+function App({ Component, pageProps, router }) {
+  const hideNav = AUTH_PAGES.some((p) => router.pathname.startsWith(p));
+  return (
+    <div className="min-h-screen bg-[#0b0d12] text-white">
+      {!hideNav && <Nav />}
+      <Component {...pageProps} />
+    </div>
+  );
 }
+export default App;
