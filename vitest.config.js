8d5cadfaabe00b962e7d8222b75b3807c1ed9e65
Drodo44 <AJC4492@gmail.com>
2025-08-19 10:12:24 -0400

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,12 @@
-// vitest.config.js
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: "node",
-    include: ["tests/**/*.test.js"],
-    globals: true
+    environment: 'node',
+    globals: true,
+    include: ['tests/**/*.test.js'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
