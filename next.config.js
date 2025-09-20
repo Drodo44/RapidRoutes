dbacdda588a960b0139f434fec5d395550acef7c
Drodo44 <AJC4492@gmail.com>
2025-09-20 15:41:48 +0000

    fix: Simplify Next.js config to avoid conflicts

--- next.config.js
+++ next.config.js
@@ -4,21 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.extensions = ['.js', '.jsx', '.json', '.ts', '.tsx']
-    config.resolve.extensionAlias = {
-      '.js': ['.js', '.jsx', '.ts', '.tsx'],
-      '.jsx': ['.jsx']
-    }
-    config.module = {
-      ...config.module,
-      rules: [
-        ...config.module.rules,
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        }
-      ]
-    }
     return config
   }
 }
