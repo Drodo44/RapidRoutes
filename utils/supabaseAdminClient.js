7652e96b520d5334132b9c2186875b65eb0712dc
Drodo44 <AJC4492@gmail.com>
2025-09-29 03:52:56 +0000

    fix(intelligence): add named export for adminSupabase wrapper to resolve build import error

--- utils/supabaseAdminClient.js
+++ utils/supabaseAdminClient.js
@@ -1,4 +1,10 @@
 // utils/supabaseAdminClient.js
 // Wrapper to provide a default export for adminSupabase (originally a named export)
 import { adminSupabase } from './supabaseClient.js';
+
+// Provide both default and named export so modules can import either:
+// import adminSupabase from './supabaseAdminClient';
+// or
+// import { adminSupabase } from './supabaseAdminClient';
+export { adminSupabase };
 export default adminSupabase;
